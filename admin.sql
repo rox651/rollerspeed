2d216698542e1453659ececdfe9eef08560fd329
Homero Bracamonte <homerojose69@gmail.com>
2025-04-06 22:14:38 -0500

    (fix): update hashed password for initial admin user in admin.sql

--- admin.sql
+++ admin.sql
@@ -1,10 +1,9 @@
--- Insert initial admin user
 -- Password: admin123 (hashed with BCrypt)
 INSERT INTO administrators (nombre, apellido, email, password, is_active)
 VALUES (
     'Admin',
     'Principal',
     'admin@rollerspeed.com',
-    '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi',
+    '$2a$10$oEjsWUW1NPkM6sJgJWH2fev9.pz34DGv5GLaHCa8gcZISEz7vlnHW',
     true
 ); 
